649c86f180a3dc010f642795d62e822bcc2978c4
Paula Arnaiz <paula.arnaiz@alumni.mondragon.edu>
2023-03-05 20:35:36 +0100

    flask

--- Flask/server.py
+++ Flask/server.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask,  render_template, request, session
 
 
 app = Flask(__name__)
 
+app.secret_key = 'jshugk'
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('layout.html')
+
+@app.route('/form', methods = ['POST', 'GET']) 
+def formulario():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellidos = request.form['apellidos'] 
+        session['nombre'] = nombre
+        session['apellidos'] = apellidos
+        if (len(session['nombre']) == 0) & (len(session['apellidos']) == 0):
+            mens = 'Por favor, introduzca su nombre y apellidos.'
+            return render_template("form.html", mens = mens)    
+        
+        genero = request.form['genero']
+        if genero == 'Mujer':
+            ending = 'a'
+        elif genero == 'Hombre':
+            ending = 'o'
+        else:
+            ending = 'x'
+        mens = f'Bienvenid{ending}, {nombre} {apellidos}.'
+
+        return render_template("home.html", 
+                             mens = mens)
+    else:
+        return render_template("form.html")
 
 @app.route('/importarcsv', methods=['GET','POST'])
 def importarcsv():
